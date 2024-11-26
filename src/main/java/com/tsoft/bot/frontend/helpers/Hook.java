841aea0ef43824fbdee536addce736ab50a5ddb0
prvalfobre0 <alfredo.obregon@inetum.com>
2024-11-26 15:51:17 -0500

    mejoras3

--- src/main/java/com/tsoft/bot/frontend/helpers/Hook.java
+++ src/main/java/com/tsoft/bot/frontend/helpers/Hook.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.TimeoutException; // Línea añadida
+
 //
 import com.tsoft.bot.frontend.listener.Listener;
 import com.tsoft.bot.both.utility.FileHelper;
